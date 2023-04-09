8e5ff15d29afb07fd5d822f8ffa1f5dd6eb241ac
Kaustav-Goswami9 <kaustavhome23@gmail.com>
2023-04-09 17:02:34 +0530

    Formatted files.

--- src/router/index.js
+++ src/router/index.js
@@ -7,45 +7,20 @@ import DashBoardView from "../views/DashBoardView.vue";
 import SubjectView from "../views/SubjectView.vue";
 
 const routes = [
+  { path: "/", name: "home", component: HomeView, },
   {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
+    path: "/about", name: "about",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
-  {
-    path: "/register",
-    name: "register",
-    component: RegisterStudent,
-  },
-  {
-    path: "/enroll",
-    name: "enroll",
-    component: RegisterStaff,
-  },
-  {
-    path: "/test",
-    name: "test",
-    component: TestingAPI,
-  },
-  {
-    path: "/dash",
-    name: "dashboard",
-    component: DashBoardView,
-  },
-  {
-    path: "/subject/:subject",
-    component: SubjectView,
-    name: "Subject Dashboard",
-  },
+  { path: "/register", name: "register", component: RegisterStudent, },
+  { path: "/enroll", name: "enroll", component: RegisterStaff, },
+  { path: "/test", name: "test", component: TestingAPI, },
+  { path: "/dash", name: "dashboard", component: DashBoardView, },
+  { path: "/subject/:subject", component: SubjectView, name: "Subject Dashboard", },
 ];
 
 const router = createRouter({
