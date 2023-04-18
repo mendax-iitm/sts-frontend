59929df8d5ba165a53b2a6dc08dc8e222636d845
Kaustav-Goswami9 <kaustavhome23@gmail.com>
2023-04-18 20:02:40 +0530

    Admin Search function working.

--- src/views/RoleView.vue
+++ src/views/RoleView.vue
@@ -10,17 +10,18 @@
                 id="btnradio2" />
             <label class="btn btn-outline-primary" for="btnradio2">UnApproved</label>
         </span>
-        <form class="search" @submit.prevent="search_function" role="search">
-            <input class="search" type="text" id="search" placeholder="Search Username here..." v-model="search" />
+        <form class="search" role="search">
+            <input class="search" type="text" id="search" placeholder="Search Username here..." v-model="search"
+                @input="search_function" />
             <button type="submit" class="btn btn-link"> <i class="bi bi-search"></i> </button>
         </form>
         <div class="container pt-2">
-            <div class="text-center" v-if="!role_list.length">
+            <div class="text-center" v-if="!filtered_list.length">
                 <img src="../assets/notFound.jpg" alt="No image found">
                 <h3>No Staff data found under this section.</h3>
             </div>
             <div v-else>
-                <div class="row m-1" v-for="(role, index) in role_list" :key="index">
+                <div class="row m-1" v-for="(role, index) in filtered_list" :key="index">
                     <div class="card position-relative" style="width: 84%; left: 5rem; min-height: 4em">
                         <div style="font-size: 1.5em; width: 90%; margin-left: 2.5em" class="d-flex mt-1">
                             <div class="flex-grow-1 justify-content-start">
@@ -35,7 +36,7 @@
                                 <button class="btn btn-danger" @click="DelRole">Delete</button>
                             </div>
                             <div class="align-self-center justify-content-end" v-else>
-                                <button class="btn btn-success"
+                                <button class="btn btn-success me-2"
                                     @click="ApproveRole(role.user_id, role.subject_id)">Approve</button>
                                 <button class="btn btn-danger" @click="DelRole(role.user_id)">Deny</button>
                             </div>
@@ -61,12 +62,16 @@ export default {
     data: function () {
         return {
             role_list: [],
+            filtered_list: [],
             search: '',
             selectedOption: "approved",
             role: localStorage.getItem("role")
         };
     },
     methods: {
+        search_function() {
+            this.filtered_list = this.role_list.filter(x => x.username.toLowerCase().includes(this.search.toLowerCase()))
+        },
         APPROVED() {
             fetch(`http://127.0.0.1:5500/api/role?status=1`, {
                 method: 'GET',
@@ -79,6 +84,7 @@ export default {
                 .then(response => response.json())
                 .then((data) => {
                     this.role_list = data;
+                    this.filtered_list = data;
                 })
                 .catch((err) => console.log(err));
         },
@@ -94,6 +100,7 @@ export default {
                 .then(response => response.json())
                 .then((data) => {
                     this.role_list = data;
+                    this.filtered_list = data;
                 })
                 .catch((err) => console.log(err));
         },
@@ -105,8 +112,9 @@ export default {
                         "Access-Control-Allow-Origin": "*",
                         Authorization: "Bearer " + localStorage.getItem("access_token"),
                     }
-                })
-                    .catch((err) => console.log(err));
+                }).catch((err) => console.log(err));
+                this.role_list = this.role_list.filter(x => x.user_id != user_id)
+                this.search_function()
             }
         },
         ApproveRole(user_id, subject_id) {
